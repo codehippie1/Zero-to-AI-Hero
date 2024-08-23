5c45aecceb3e43e51507547b6dd5aee851812c10
codehippie1 <codehippie1@gmail.com>
2024-08-22 22:11:08 -0400

    Part 1

---
+++ Part 1 - The Beginning (SK)/Program.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.SemanticKernel;
+
+var builder = Kernel.CreateBuilder();
+builder.AddAzureOpenAIChatCompletion(
+    deploymentName: "open-ai-gtp4o-poc-ag-deployment",
+    endpoint: "https://open-ai-gtp4o-poc-ag.openai.azure.com/",
+    apiKey: "508bf29262e04363a4d4811b910d3e4d"
+);
+var kernel = builder.Build();
+
+Console.WriteLine(await kernel.InvokePromptAsync("What is Gen AI?"));
