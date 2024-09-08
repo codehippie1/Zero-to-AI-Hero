c9bf552acf965703a9fafe441e4d0ebffa043611
codehippie1 <codehippie1@gmail.com>
2024-09-07 23:22:54 -0400

    Part 4 - First cut

---
+++ Part 4 - Local Models/Program.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.SemanticKernel;
+# pragma warning disable SKEXP0010
+
+var builder = Kernel.CreateBuilder();
+builder.AddOpenAIChatCompletion(
+    modelId: "phi3",
+    apiKey: null,
+    endpoint: new Uri("http://localhost:11434")
+);
+
+var kernel = builder.Build();
+
+Console.Write("User: ");
+var input = Console.ReadLine();
+var response = await kernel.InvokePromptAsync(input);
+Console.WriteLine(response.GetValue<string>());
+Console.WriteLine("------------------------------------------------------------------------");
+Console.ReadLine();
